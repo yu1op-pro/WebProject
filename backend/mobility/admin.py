fc8a0bed96136d6e87549044900f5e6e423c321b
Amina <153654222+yu1op-pro@users.noreply.github.com>
2026-04-18 22:08:42 +0500

    Delete old backend directory

--- backend/mobility/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import University,Faculty, Application, Review
-
-admin.site.register(University)
-admin.site.register(Faculty)
-admin.site.register(Application)
-admin.site.register(Review)
--- backend/mobility/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MobilityConfig(AppConfig):
-    name = 'mobility'
